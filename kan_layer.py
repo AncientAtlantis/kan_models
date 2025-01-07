6e97058a873010bcd39eff1a142cd5a15a9be250
bolin yang <boliny@163.com>
2025-01-08 00:15:38 +0800

    add

--- kan_layer.py
+++ kan_layer.py
@@ -1,5 +1,48 @@
 import tensorflow as tf
 
-class KanLayer(tf.Module):
-    def __init__(self,):
+
+class SegmentKanLayer(tf.Module):
+    """
+    The
+    """
+    def __init__(self,
+                 in_size,
+                 out_size,
+                 num_grids,
+                 delta=1e-8,
+                 initializer,
+                 name_prefix='SegmentKanLayer'):
+        super().__init__(name=name_prefix)
+        self.in_size=in_size
+        self.out_size=out_size
+        self.num_grids=num_grids
+        self.delta=delta
+        self.initializer=initializer
+        self.name_prefix=name_prefix    
+        self.is_build=False
+    
+    def build(self):
+        if not self.is_build:
+            
+            self.is_build=True
+
+
+class FourierKanLayer(tf.Module):
+    def __init__(self,
+                 out_size,
+                 base_function,
+
+                 name=None):
+        super().__init__(name=None)
+
+        pass
+
+class SplineKanLayer(tf.Module):
+    def __init__(self,
+                 out_size,
+                 base_function,
+
+                 name=None):
+        super().__init__(name=None)
+
         pass
